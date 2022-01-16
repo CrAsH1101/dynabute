8c1dc328396a76856dd97502df82bb1893929fd1
Igor Grubisic <igrubisic@gmail.com>
2022-01-17 00:08:27 +0100

    Automatically build missing values for valid fields.
    Allow update value for fields that do not have inserted record in
    appropriate value table.

--- spec/dummy/spec/value_spec.rb
+++ spec/dummy/spec/value_spec.rb
@@ -114,6 +114,212 @@ RSpec.describe Dynabute::Dynabutable, type: :model do
         end
       end
     end
+
+    describe '#get_dynabute_value' do
+      context 'has one' do
+        context 'when value record exists' do
+          let!(:value) { Dynabute::Values::IntegerValue.create(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_field.id, value: 42)}
+          it 'returns literal value found by field' do
+            expect(user.get_dynabute_value(field: int_field)).to eq(42)
+          end
+          it 'returns literal value found by field name' do
+            expect(user.get_dynabute_value(name: int_field.name)).to eq(42)
+          end
+          it 'returns literal value found by field id' do
+            expect(user.get_dynabute_value(field_id: int_field.id)).to eq(42)
+          end
+        end
+        context 'when value record does not exist' do
+          it 'returns nil' do
+            expect(user.get_dynabute_value(field: int_field)).to be_nil
+          end
+        end
+      end
+      context 'has_many' do
+        let!(:int_many_field) { Dynabute::Field.create(name: 'int many field', value_type: :integer, target_model: 'User', has_many: true) }
+        context 'when value records exist' do
+          let!(:first_value) { Dynabute::Values::IntegerValue.create!(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_many_field.id, value: 1) }
+          let!(:second_value) { Dynabute::Values::IntegerValue.create!(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_many_field.id, value: 2) }
+          context 'value_id argument is not provided' do
+            it 'returns array of literal values' do
+              expect(user.get_dynabute_value(field: int_many_field)).to match_array([1, 2])
+            end
+          end
+          context 'value_id argument is provided' do
+            it 'returns literal value for given value record' do
+              expect(user.get_dynabute_value(field: int_many_field, value_id: second_value.id)).to eq(2)
+            end
+          end
+        end
+        context 'when value records do not exist' do
+          context 'value_id argument is not provided' do
+            it 'returns empty array' do
+              expect(user.get_dynabute_value(field: int_many_field)).to match_array([])
+            end
+          end
+          context 'value_id argument is provided' do
+            it 'returns nil' do
+              expect(user.get_dynabute_value(field: int_many_field, value_id: 999)).to be_nil
+            end
+          end
+        end
+      end
+    end
+  end
+
+  describe 'setting value' do
+    let!(:user) { User.create }
+    context 'has_one' do
+      let!(:int_field) { Dynabute::Field.create(name: 'int field', value_type: :integer, target_model: 'User') }
+      context 'when value record does not exist' do
+        subject { user.set_dynabute_value(field: int_field, value: 42) }
+        it 'creates new unsaved association' do
+          expect(subject).to be_a(Dynabute::Values::IntegerValue)
+          expect(subject.attributes).to include({
+            'field_id' => int_field.id,
+            'dynabutable_type' => 'User',
+            'dynabutable_id' => user.id,
+            'value' => 42,
+            'id' => nil
+          })
+          expect(subject.new_record?).to be true
+        end
+      end
+      context 'when value record exists' do
+        let!(:value) { Dynabute::Values::IntegerValue.create(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_field.id, value: 1)}
+        subject { user.set_dynabute_value(field: int_field, value: 42) }
+        it 'changes existing association without saving' do
+          expect(subject).to be_a(Dynabute::Values::IntegerValue)
+          expect(subject.attributes).to include({
+            'field_id' => int_field.id,
+            'dynabutable_type' => 'User',
+            'dynabutable_id' => user.id,
+            'value' => 42,
+            'id' => value.id
+          })
+          expect(subject.persisted?).to be true
+        end
+      end
+    end
+    context 'has_many' do
+      let!(:int_many_field) { Dynabute::Field.create(name: 'int many field', value_type: :integer, target_model: 'User', has_many: true) }
+      context 'when value records do not exist' do
+        context 'when value_id argument is not provided' do
+          subject { user.set_dynabute_value(field: int_many_field, value: 42) }
+          it 'creates new unsaved association' do
+            expect(subject).to be_a(Dynabute::Values::IntegerValue)
+            expect(subject.attributes).to include({
+              'field_id' => int_many_field.id,
+              'dynabutable_id' => user.id,
+              'dynabutable_type' => 'User',
+              'value' => 42,
+              'id' => nil
+            })
+            expect(subject.new_record?).to be true
+          end
+        end
+        context 'when value_id argument is provided' do
+          subject { user.set_dynabute_value(field: int_many_field, value: 42, value_id: 999) }
+          it 'raises ValueNotFound exception' do
+            expect { subject }.to raise_error(Dynabute::Dynabutable::ValueNotFound)
+          end
+        end
+      end
+      context 'when value records exist' do
+        let!(:first_value) { Dynabute::Values::IntegerValue.create!(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_many_field.id, value: 1) }
+        let!(:second_value) { Dynabute::Values::IntegerValue.create!(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_many_field.id, value: 2) }
+        context 'when value_id argument is not provided' do
+          subject { user.set_dynabute_value(field: int_many_field, value: 42) }
+          it 'created new unsaved association' do
+            expect(subject).to be_a(Dynabute::Values::IntegerValue)
+            expect(subject.attributes).to include({
+              'field_id' => int_many_field.id,
+              'dynabutable_type' => 'User',
+              'dynabutable_id' => user.id,
+              'value' => 42,
+              'id' => nil
+            })
+            expect(subject.new_record?).to be true
+          end
+        end
+        context 'when value_id argument is provided' do
+          subject { user.set_dynabute_value(field: int_many_field, value: 42, value_id: second_value.id) }
+          it 'changes existing association without saving' do
+            expect(subject).to be_a(Dynabute::Values::IntegerValue)
+            expect(subject.attributes).to include({
+              'field_id' => int_many_field.id,
+              'dynabutable_type' => 'User',
+              'dynabutable_id' => user.id,
+              'value' => 42,
+              'id' => second_value.id
+            })
+            expect(subject.persisted?).to be true
+          end
+        end
+      end
+    end
+  end
+
+  describe 'deleting value' do
+    let!(:user) { User.create }
+    context 'has_one' do
+      let!(:int_field) { Dynabute::Field.create(name: 'int field', value_type: :integer, target_model: 'User') }
+      context 'when value record exists' do
+        let!(:value) { Dynabute::Values::IntegerValue.create(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_field.id, value: 1)}
+        subject { user.remove_dynabute_value(field: int_field) }
+        it 'returns deleted association' do
+          expect(subject).to be_a(Dynabute::Values::IntegerValue)
+          expect(subject.attributes).to include({
+            'field_id' => value.field_id,
+            'dynabutable_type' => value.dynabutable_type,
+            'dynabutable_id' => value.dynabutable_id,
+            'value' => value.value,
+            'id' => value.id
+          })
+          expect(subject.destroyed?).to be true
+        end
+      end
+      context 'when value record does not exist' do
+        subject { user.remove_dynabute_value(field: int_field) }
+        it 'returns nil' do
+          expect(subject).to be_nil
+        end
+      end
+    end
+    context 'has_many' do
+      let!(:int_many_field) { Dynabute::Field.create(name: 'int many field', value_type: :integer, target_model: 'User', has_many: true) }
+      context 'when value records exist' do
+        let!(:first_value) { Dynabute::Values::IntegerValue.create!(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_many_field.id, value: 1) }
+        let!(:second_value) { Dynabute::Values::IntegerValue.create!(dynabutable_id: user.id, dynabutable_type: 'User', field_id: int_many_field.id, value: 2) }
+        context 'when value_id argument is not provided' do
+          subject { user.remove_dynabute_value(field: int_many_field) }
+          it 'returns array of deleted associations' do
+            expect(subject).to all(be_a(Dynabute::Values::IntegerValue))
+            expect(subject.map(&:destroyed?)).to all(be true)
+          end
+        end
+        context 'when value_id argument is provided' do
+          subject { user.remove_dynabute_value(field: int_many_field, value_id: second_value.id) }
+          it 'returns deleted association' do
+            expect(subject).to be_a(Dynabute::Values::IntegerValue)
+            expect(subject.attributes).to include({
+              'field_id' => second_value.field_id,
+              'dynabutable_type' => second_value.dynabutable_type,
+              'dynabutable_id' => second_value.dynabutable_id,
+              'value' => second_value.value,
+              'id' => second_value.id
+            })
+            expect(subject.destroyed?).to be true
+          end
+        end
+      end
+      context 'when value records do not exist' do
+        subject { user.remove_dynabute_value(field: int_many_field) }
+        it 'returns empty array' do
+          expect(subject).to match_array([])
+        end
+      end
+    end
   end
 
   describe '#build_dynabute_value' do
